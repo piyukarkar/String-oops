9fdbd857ebdc61766b5b762ebe031aaf80c107e0
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-24 23:18:43 +0530

    .

--- String/StringIntro.java
+++ String/StringIntro.java
@@ -1,61 +1,60 @@
-public class StringIntro {
-    public static void main(String[] args) {
+public class StringIntro;
+public static void main(String[] args) {
 		
-        //		String name = "Priyanka";  //pool area
-        //		String name2 = "Priyanka";
-        //		
-        //		String anotherName = new String ("Aman"); //heap area
-        //		
-        //		System.out.println(name==name2);
+    //		String name = "Priyanka";  //pool area
+    //		String name2 = "Priyanka";
+    //		
+    //		String anotherName = new String ("Aman"); //heap area
+    //		
+    //		System.out.println(name==name2);
+            
+    //		String name = "Priyanka Karkar";
+            
+    //			System.out.println(name.charAt(0));
+            
+    //		 System.out.println(name.length());
+            
+    //		System.out.println(name.substring(5));
+            
+    //		System.out.println(name.substring(0,17));
+            
+    //		System.out.println(name.contains("aman"));
+            
+    //		String name = "Priyanka";
+    //		
+    //		String name2 = new String("Priyanka");
+    //		
+    //		System.out.println(name.equals(name2));
+            
+    //		String name = "";
+            
+    //		System.out.println(name.isEmpty());
+            
+    //		String name = "Priyanka";
+    //		name = name + " Karkar";
+            
+    //		System.out.println(name.concat(" miss"));
+    //		
+    //		System.out.println(name.replace('A','c'));
+            
+            String cars ="Huyndai,Maruti,Swift,Wagonr,Farrari,Lamborghini";
+            
+    //		String allCars[] = cars.split(",");
+    //		
+    //		for(String car: allCars) {
+    //			System.out.println(car);}
+            
+    //		System.out.println(cars.indexOf('M'));
+            
+    //		System.out.println(cars.toLowerCase());
+            
+            System.out.println(cars.toUpperCase());
+            
+    //		String name = "       Priyanka     ";
+    //		
+    //		System.out.println( name.trim());
                 
-        //		String name = "Priyanka Karkar";
-                
-        //			System.out.println(name.charAt(0));
-                
-        //		 System.out.println(name.length());
-                
-        //		System.out.println(name.substring(5));
-                
-        //		System.out.println(name.substring(0,17));
-                
-        //		System.out.println(name.contains("aman"));
-                
-        //		String name = "Priyanka";
-        //		
-        //		String name2 = new String("Priyanka");
-        //		
-        //		System.out.println(name.equals(name2));
-                
-        //		String name = "";
-                
-        //		System.out.println(name.isEmpty());
-                
-        //		String name = "Priyanka";
-        //		name = name + " Karkar";
-                
-        //		System.out.println(name.concat(" miss"));
-        //		
-        //		System.out.println(name.replace('A','c'));
-                
-                String cars ="Huyndai,Maruti,Swift,Wagonr,Farrari,Lamborghini";
-                
-        //		String allCars[] = cars.split(",");
-        //		
-        //		for(String car: allCars) {
-        //			System.out.println(car);}
-                
-        //		System.out.println(cars.indexOf('M'));
-                
-        //		System.out.println(cars.toLowerCase());
-                
-                System.out.println(cars.toUpperCase());
-                
-        //		String name = "       Priyanka     ";
-        //		
-        //		System.out.println( name.trim());
-                    
-                }
-                
-                
-                
-}
+            }
+            
+            
+            
