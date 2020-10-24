2a3f2df72ae1f83203ea4a2331ca5e27895ab394
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-24 11:55:36 +0530

    .

---
+++ Oops/inheritence/MainClass.java
@@ -0,0 +1,45 @@
+package oops.inheritence;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Teacher t1 = new Teacher("Mr. harry");
+		
+//		Singer s1 = new Singer();
+//		
+//		Person p = t1;
+//		
+//		Teacher t = (Teacher)p;
+//		
+//		boolean yo = t1 instanceof Teacher;
+//		
+//		System.out.println(t1 instanceof Teacher);
+//		System.out.println(s1 instanceof Singer);
+//		System.out.println(t1 instanceof Person);
+//		
+//		System.out.println(t instanceof Person);
+//		System.out.println(p instanceof Singer);
+//		
+		
+		
+//		
+//		t1.name = "mr. Harry";
+		
+//		t1.eat();
+//		
+//		t.teach();
+//		
+//		Singer s = new Singer();
+//		
+//		s.name = "Oprah";
+//		s.sing();
+//		
+//		s.eat();
+//		
+//		s.laughing();
+//
+	}
+
+}
+
