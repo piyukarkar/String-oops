6417e2e1dd030e680f907148784ef4c90797f685
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-24 12:03:34 +0530

    .

---
+++ Oops/finalKeyWord/Student.java
@@ -0,0 +1,24 @@
+package oops.finalKeyword;
+
+public final class Student {
+
+		int rollNo;
+		String name;
+		
+		public final void getDescription() {
+			System.out.println("The Student name is " +  name);
+		}	
+
+//	final int rollNo;
+//	final String name;
+//	
+//	{
+//		rollNo = 4;
+//	}
+//	
+//	public Student() {
+//		name ="Priyanka";
+//		
+//	}
+
+}
